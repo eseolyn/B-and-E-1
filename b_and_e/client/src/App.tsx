e298576831402163bf546f205c91e6981d221465
eunseolYun <skopdt73@gmail.com>
2023-05-18 18:32:38 +0900

    FIX: merge conflict

--- b_and_e/client/src/App.tsx
+++ b_and_e/client/src/App.tsx
@@ -1,23 +1,12 @@
-import React from 'react';
-<<<<<<< HEAD
-import logo from '../src/img/logo.png';
-=======
-import Routers from './Routers';
-
->>>>>>> cf663b191b6107e7957a9b9430d9b9e82eee52d6
+import React from "react";
+import Routers from "./Routers";
 
 function App() {
-  return (
-    <div className="App">
-<<<<<<< HEAD
-      <header className="App-header">
-        <img src={logo} alt="logo" />
-      </header>
-=======
-      <Routers />
->>>>>>> cf663b191b6107e7957a9b9430d9b9e82eee52d6
-    </div>
-  );
+    return (
+        <div className="App">
+            <Routers />
+        </div>
+    );
 }
 
 export default App;
